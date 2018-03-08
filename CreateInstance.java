3c6725a9388e7ce1e0b307248a9fffa97e407df9
iSandeepParbhankar <iparbhankarsandeep@gmail.com>
2018-03-08 15:12:01 +0530

    updated

--- CreateInstance.java
+++ CreateInstance.java
@@ -15,33 +15,68 @@ import java.lang.reflect.InvocationTargetException;
 
 public class CreateInstance {
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, NoSuchMethodException,
-			SecurityException, IllegalArgumentException, InvocationTargetException,
-			/**/
-			CloneNotSupportedException, 
-			FileNotFoundException, IOException, ClassNotFoundException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// Way 1: create instance using new keyword.
 		DemoClass way1 = new DemoClass();
 
 		// way2: using newInstance() method
-		DemoClass way2 = DemoClass.class.newInstance();
+		try {
+			DemoClass way2 = DemoClass.class.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// way3: using newInstance() method of Constructor class
-		Constructor<DemoClass> constructor = DemoClass.class.getConstructor();
-		DemoClass way3 = constructor.newInstance();
+		Constructor<DemoClass> constructor;
+		DemoClass way3 = null;
+		try {
+			constructor = DemoClass.class.getConstructor();
+			way3 = constructor.newInstance();
+		} catch (NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 		// way4: using clone() method
 		// note: to use clone DemoClass must implement Cloneable and define the clone
 		// method.
-		DemoClass way4 = (DemoClass) way3.clone();
+		try {
+			DemoClass way4 = (DemoClass) way3.clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// way5: using deserialization.
 		// Note: To deserialize an object we need to implement a Serializable interface
 		// in our class.
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream("data.obj"));
-		DemoClass way5 = (DemoClass) in.readObject();
+		ObjectInputStream in;
+		try {
+			in = new ObjectInputStream(new FileInputStream("data.obj"));
+			DemoClass way5 = (DemoClass) in.readObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
